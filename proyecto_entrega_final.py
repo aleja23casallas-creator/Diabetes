aa0d91d26dcce94177a1638db540c476678b4962
aleja23casallas-creator <aleja23casallas@gmail.com>
2025-08-27 23:40:15 -0500

    Update proyecto_entrega_final.py
    Quitar !pip install de script

--- proyecto_entrega_final.py
+++ proyecto_entrega_final.py
@@ -331,9 +331,9 @@ X_pca = pca.fit_transform(X_train_scaled)
 print(f"Número de componentes principales para explicar 85% varianza: {pca.n_components_}")
 print(f"Varianza explicada acumulada por estas componentes: {sum(pca.explained_variance_ratio_):.4f}")
 
-!pip install mca
+#!pip install mca
 
-!pip install prince
+#!pip install prince
 
 import pandas as pd
 import numpy as np
